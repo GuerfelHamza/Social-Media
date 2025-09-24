3648ba8553bc829d6f495ee096a284cf0b181071
guerfelhamza <nonstopcoding77@gmail.com>
2025-09-25 01:41:40 +0200

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+// ====== GESTION DU LIKE ======
+document.querySelectorAll(".btn-like").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("liked"); // toggle style rouge + animation
+    btn.classList.toggle("bi-heart"); // alterne icône coeur vide
+    btn.classList.toggle("bi-heart-fill"); // alterne icône coeur rempli
+  });
+});
